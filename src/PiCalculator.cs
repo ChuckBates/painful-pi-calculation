0ee9f8e2ce9ff0915986d5623e837391e6906c98
Chuck Bates <chuck.bates@sdl.usu.edu>
2022-03-02 14:37:29 -0700

    brute force solution

--- src/PiCalculator.cs
+++ src/PiCalculator.cs
@@ -1,11 +1,45 @@
-﻿namespace src;
+﻿
+using System.Diagnostics;
 
-public class PiCalculator
+namespace src
 {
-    public static void Main(string[] args) {}
+  public class PiCalculator
+  {
+    readonly decimal HighestPrecisionPiConstant = 3.1415926535897932384626433833m;
 
-    public ulong Calculate(double precision)
+    public static void Main(string[] args) { }
+
+    public static (ulong Iterations, decimal Value) Calculate(decimal constant, decimal precision)
     {
-        throw new NotImplementedException();
+      var stopwatch = Stopwatch.StartNew();
+
+      if (constant <= 0 || precision <= 0)
+      {
+        throw new ArgumentException("Invalid Argument: cannot be negative");
+      }
+
+      decimal calculatedValue = 4m;
+      decimal oddDenominator = 3m;
+      var positiveSign = false;
+      ulong iterationCounter = 1;
+      while (Math.Abs(calculatedValue - constant) > precision)
+      {
+        if (iterationCounter % 10000000 == 0)
+        {
+          Console.WriteLine($"Calculated for {iterationCounter} iterations in {stopwatch.Elapsed.TotalSeconds} seconds. Currently calculated value {calculatedValue} is within {Math.Abs(calculatedValue - constant)}");
+        }
+
+        var nextTerm = 4m / oddDenominator;
+        calculatedValue = positiveSign ? calculatedValue + nextTerm : calculatedValue - nextTerm;
+
+        positiveSign = !positiveSign;
+        oddDenominator += 2;
+        iterationCounter++;
+      }
+
+      Console.WriteLine($"{iterationCounter} iterations in {stopwatch.Elapsed.TotalSeconds} seconds needed to calculate Pi value {calculatedValue} to within {precision} of constant");
+
+      return (iterationCounter, calculatedValue);
     }
+  }
 }
--- tests/PiCalculatorTests.cs
+++ tests/PiCalculatorTests.cs
@@ -1,21 +1,68 @@
+using System;
 using NUnit.Framework;
 using src;
 
-namespace tests;
-
-public class Tests
+namespace tests
 {
-    PiCalculator ClassUnderTest;
-    
-    [SetUp]
-    public void Setup()
+  public class Tests
+  {
+    readonly decimal PiConstant = (decimal)Math.PI;
+
+    [Test]
+    public void When_evaluating_with_ultra_low_precision()
     {
-        ClassUnderTest = new PiCalculator();
+      var expected = 1;
+      var results = PiCalculator.Calculate(PiConstant, 1m);
+
+      Assert.That(results, Is.Not.Null);
+      Assert.That(results.Iterations, Is.Not.Null);
+      Assert.That(results.Iterations, Is.EqualTo(expected));
+      Assert.That(results.Value, Is.EqualTo(4m));
     }
 
     [Test]
-    public void When_making_sure_bad_things_dont_happen()
+    public void When_evaluating_with_low_precision()
     {
-        Assert.DoesNotThrow(() => ClassUnderTest.Calculate(0.001));
+      var precision = 0.01m;
+      var expected = 100;
+      var results = PiCalculator.Calculate(PiConstant, precision);
+
+      Assert.That(results.Iterations, Is.EqualTo(expected));
+      Assert.That(results.Value, Is.EqualTo(PiConstant).Within(precision));
     }
+
+    [Test]
+    public void When_evaluating_with_moderate_precision()
+    {
+      var precision = 0.00001m;
+      var expected = 100000;
+      var results = PiCalculator.Calculate(PiConstant, precision);
+
+      Assert.That(results.Iterations, Is.EqualTo(expected));
+      Assert.That(results.Value, Is.EqualTo(PiConstant).Within(precision));
+    }
+
+    [Test]
+    public void When_evaluating_with_moderate_high_precision()
+    {
+      var precision = 0.0000001m;
+      var expected = 10000000;
+      var results = PiCalculator.Calculate(PiConstant, precision);
+
+      Assert.That(results.Iterations, Is.EqualTo(expected));
+      Assert.That(results.Value, Is.EqualTo(PiConstant).Within(precision));
+    }
+
+    [Test]
+    public void When_evaluating_with_invalid_constant()
+    {
+      Assert.Throws<ArgumentException>(() => PiCalculator.Calculate(-3.14m, 0.01m));
+    }
+
+    [Test]
+    public void When_evaluating_with_invalid_precision()
+    {
+      Assert.Throws<ArgumentException>(() => PiCalculator.Calculate(3.14m, -0.01m));
+    }
+  }
 }
